18b7cf5c0a5be0b001e26329bb81ba9c8f0e8c5e
keshav6387 <127872755+keshav6387@users.noreply.github.com>
2023-03-30 06:03:13 +0000

    keshav

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product=require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 
 
 //creating a new product --Admin
@@ -16,10 +17,14 @@ const createProduct=catchAsyncErrors(async(req,res,next)=>{
 //Get all products
 
 const getAllProducts=catchAsyncErrors(async(req,res)=>{
-    const products=await Product.find();
+    const resultPerPage=5
+    const productCount= await Product.countDocuments();
+    const apiFeature= new ApiFeatures(Product.find(),req.query).serach().filter().pagination(resultPerPage)
+    const products=await apiFeature.query;
     res.status(200).json({
         success:true,
-        products
+        products,
+        productCount
     })
 })
 
